64e99102dd7a6e5cfc3e99e0f909261fd966c935
DESKTOP-7K61OQD\USER <yshss1@naver.com>
2021-04-30 11:29:57 +0900

    수정되었습니다

---
+++ webprj/src/main/java/com/comes/web/controller/notice/DetailController.java
@@ -0,0 +1,21 @@
+package com.comes.web.controller.notice;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class DetailController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		System.out.println("Controller");
+		ModelAndView mv = new ModelAndView("notice/detail");
+		mv.addObject("data", "Hello Spring MVC~");
+		// mv.setViewName("/WEB-INF/view/index.jsp");
+
+		return mv;
+	}
+}
