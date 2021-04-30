4355e1b5cf73fb1a91524c5b34631e8e6851b2c2
DESKTOP-7K61OQD\USER <yshss1@naver.com>
2021-04-30 18:19:02 +0900

    oracle db연동 준비로 먼저 foreach를 하귀 위해 jstl로 core를 불러왔음 또한 service 패키지를 추가함

--- webprj/src/main/java/com/comes/web/controller/notice/ListController.java
+++ webprj/src/main/java/com/comes/web/controller/notice/ListController.java
@@ -1,20 +1,36 @@
 package com.comes.web.controller.notice;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
+import com.comes.web.service.NoticeService;
+import com.comes.web.service.entity.Notice;
+
+
 public class ListController implements Controller {
+	
+	private NoticeService noticeService;
+	
+
+	public void setNoticeService(NoticeService noticeService) {
+		this.noticeService = noticeService;
+	}
+
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
 		System.out.println("Controller");
 		ModelAndView mv = new ModelAndView("notice.list");
-		mv.addObject("data","Hello Spring MVC~");
-		//mv.setViewName("/WEB-INF/view/index.jsp");
+		
+		List<Notice> list=noticeService.getList(1, "TITLE", "");
+		mv.addObject("list",list);
+	
 		
 		
 		
