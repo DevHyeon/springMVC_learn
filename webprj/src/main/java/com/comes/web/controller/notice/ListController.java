d6ebe89cf3d1d71ace4ce4a331e8929f2dfecec2
DESKTOP-7K61OQD\USER <yshss1@naver.com>
2021-05-03 16:55:23 +0900

    NoticeService -> JDBCNoticeService로 변경 conn xml형식으로 데이터 받아 연동

--- webprj/src/main/java/com/comes/web/controller/notice/ListController.java
+++ webprj/src/main/java/com/comes/web/controller/notice/ListController.java
@@ -11,29 +11,23 @@ import org.springframework.web.servlet.mvc.Controller;
 import com.comes.web.service.NoticeService;
 import com.comes.web.service.entity.Notice;
 
-
 public class ListController implements Controller {
-	
+
 	private NoticeService noticeService;
-	
 
 	public void setNoticeService(NoticeService noticeService) {
 		this.noticeService = noticeService;
 	}
-
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
 		System.out.println("Controller");
 		ModelAndView mv = new ModelAndView("notice.list");
-		
-		List<Notice> list=noticeService.getList(1, "TITLE", "");
-		mv.addObject("list",list);
-	
-		
-		
-		
+
+		List<Notice> list = noticeService.getList(1, "TITLE", "");
+		mv.addObject("list", list);
+
 		return mv;
 	}
 
